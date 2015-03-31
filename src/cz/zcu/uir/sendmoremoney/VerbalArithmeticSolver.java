1adf7227ad632fdb80d6e78cf813856397eabdf7
Jan Dvorak <fatalerror50@gmail.com>
2015-03-31 11:17:41 +0200

    created interface for VASolvers

---
+++ src/cz/zcu/uir/sendmoremoney/VerbalArithmeticSolver.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package cz.zcu.uir.sendmoremoney;
+
+/**
+ * @author Jan Dvorak A13B0293P
+ * Interface representing abstract verbal arithmetic solver
+ */
+public interface VerbalArithmeticSolver {
+	
+	/**
+	 * Method which solves VerbalArithmetic problem by implemented algorithm
+	 * @param expression verbal arithmetic expression
+	 * @return solved arangment of values for given expression
+	 */
+	VerbalArithmetic solveVerbalArithmetic(String expression);
+}
